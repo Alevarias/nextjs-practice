f4f04c0dbee89ae10111d80e0dd962eaf76da7be
Alex Sierra <alexjusticesierra@gmail.com>
2026-07-30 13:47:42 -0600

    updated footer to reflect current year and author name

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     
     <footer className="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4">
         <div className="w-full mx-auto max-w-7xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-body sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+        <span className="text-sm text-body sm:text-center">© 2026 Alexander J Sierra. All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
             {/* <li>
