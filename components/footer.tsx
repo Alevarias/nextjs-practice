430c30b93f7b182d02b40f51ed51e3944f155838
Alex Sierra <alexjusticesierra@gmail.com>
2026-08-04 10:15:43 -0600

    added linkedin and github links to footer with official images.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,28 +1,23 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    
-    <footer className="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4">
-        <div className="w-full mx-auto max-w-7xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-body sm:text-center">© 2026 Alexander J Sierra. All Rights Reserved.
-        </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
-            {/* <li>
-                <a href="#" className="hover:underline me-4 md:me-6">About</a>
-            </li>
-            <li>
-                <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" className="hover:underline">Contact</a>
-            </li> */}
-        </ul>
+    <footer className="bg-gray-200 rounded-base shadow-xs border border-b-cyan-950 border-black flex flex-col space-x-4 p-4">
+        <div className="flex flex-row space-x-4 p-4">
+            <div className="flex flex-col space-y-2 align-start">
+                <span className="font-bold">Contact</span>
+                <Link href="mailto:alexjusticesierra@gmail.com" className="hover:underline">alexjusticesierra@gmail.com</Link>
+                <Link href="https://www.linkedin.com/in/alex-j-sierra/" className="hover:underline">
+                  <Image src="/LI-Logo.png" alt="LinkedIn Link" width={100} height={100} className="object-cover"/>
+                </Link>
+                <Link href="https://github.com/Alevarias?tab=repositories" className="hover:underline">
+                  <Image src="/GitHub_Lockup_Black.svg" alt="LinkedIn Link" width={100} height={100} className="object-cover"/>
+                </Link>
+            </div>
         </div>
+        <span className="text-sm text-body sm:text-center">© 2026 Alexander J Sierra. All Rights Reserved.</span>
     </footer>
 
   );
