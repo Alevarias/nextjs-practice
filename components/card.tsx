60b324fa161f4d2920e9dae2704d149a5183969a
Alex Sierra <alexjusticesierra@gmail.com>
2026-08-12 11:31:02 -0600

    updated card component to use the new project system path

--- components/card.tsx
+++ components/card.tsx
@@ -12,7 +12,7 @@ export default function Card({id, userId, title, body}: CardProps) {
   return ( 
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <div className="px-6 py-4">
-            <Link href={`/posts/${id}`} className="font-bold text-xl mb-2 hover:underline">{title}</Link>
+            <Link href={`/projects/posts/${id}`} className="font-bold text-xl mb-2 hover:underline">{title}</Link>
             <p className="text-gray-700 text-base">{body}</p>
         </div>
     </div>
