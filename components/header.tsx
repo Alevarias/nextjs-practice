6ccde2ed4554e450fe5416d6144badc465ac33e3
Alex Sierra <alexjusticesierra@gmail.com>
2026-08-12 11:30:25 -0600

    changed header css

--- components/header.tsx
+++ components/header.tsx
@@ -4,16 +4,16 @@ import React from "react";
 export default function Header() { /** Remember to use PascalCase for functions. */
   return (
     <header className="flex items-center justify-between bg-gray-800 text-white p-4">
-          <nav className="bg-gray-800 text-white p-4">
-            <Link href="/" className="hover:text-zinc-50">
+          <nav className="bg-gray-800 text-white p-4 font-serif">
+            <Link href="/" className=" hover:text-zinc-300">
               Home
             </Link>
             <span className="mx-2">|</span>
-            <Link href="/projects" className="hover:text-zinc-50" >
+            <Link href="/projects" className="hover:text-zinc-300" >
               Web Projects
             </Link>
             <span className="mx-2">|</span>
-            <Link href="/donations" className="hover:text-zinc-50">Donations</Link>
+            <Link href="/donations" className="hover:text-zinc-300">Donations</Link>
           </nav>
     </header>
   );
