84dce3112340a80e3be7be854f2c632af3b0676a
Alex Sierra <alexjusticesierra@gmail.com>
2026-08-13 10:50:50 -0600

    removed non-existing tailwind tag to fix project card spacing when flex wrapping.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,8 +3,8 @@ import { PROJECT_LIBRARY, ProjectMetaData } from "@/lib/projects"
 export default async function ProjectsPage() {
 
     return (
-        <div className="flex flex-row align-start justify-start gap-4 p-1 min-h-fit">
-            <div className="flex flex-col space-y-4 p-4 bg-transparent rounded-lg shadow-md min-h-400">
+        <div className="flex flex-row items-start justify-start gap-4 p-1 min-h-fit">
+            <div className="flex flex-col space-y-4 p-4 bg-transparent rounded-lg shadow-md min-h-200">
                 <div className="min-h-full min-w-50">
                     <span className="font-bold text-2xl">Search</span>
                 </div>
