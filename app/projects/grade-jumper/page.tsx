a706e866580b9d1dde3321e2e6bc2f4f8800005b
Alex Sierra <alexjusticesierra@gmail.com>
2026-08-20 13:39:55 -0600

    started a small game project

---
+++ app/projects/grade-jumper/page.tsx
@@ -0,0 +1,19 @@
+
+export default async function GradeJumperPage() {
+
+    return (
+        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <h1 className="text-3xl font-bold mt-5">Grade Jumper</h1>
+            <p className="text-lg text-gray-700 dark:text-gray-300 mx-10">
+                A simple game to test your skills.
+            </p>
+            
+
+
+
+
+
+
+        </div>
+    )
+}
