030f04f9d34b9773499ac7904f1038f6da72237c
Alex Sierra <alexjusticesierra@gmail.com>
2026-08-20 13:38:38 -0600

    updated the header to include my typical icon in the nav

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image"
 
 export default function Header() { /** Remember to use PascalCase for functions. */
   return (
-    <header className="flex items-center justify-between bg-gray-800 text-white p-4 text-xl">
-          <nav className="bg-gray-800 text-white p-4 font-serif">
+    <header className="flex items-center justify-between bg-gray-800 text-white p-4 text-2xl">
+          <nav className="flex items-center bg-gray-800 text-white font-serif">
+            <Link href="/" className=" hover:text-zinc-300 mr-7">
+              <Image src="/beach-icon.png" width={70} height={70} alt="Picture of a beach sunset" />
+            </Link>
+
             <Link href="/" className=" hover:text-zinc-300">
               Home
             </Link>
